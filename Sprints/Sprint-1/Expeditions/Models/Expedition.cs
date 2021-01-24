84be1f7f7af21294104a82022f70119747e7ea9e
Adnan Almarzooq <cj-21-amm@hotmail.com>
2021-01-24 09:28:24 -0800

    adds models from DataBase.

---
+++ Sprints/Sprint-1/Expeditions/Models/Expedition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Expeditions.Models
+{
+    public partial class Expedition
+    {
+        public int Id { get; set; }
+        public string Season { get; set; }
+        public int? Year { get; set; }
+        public DateTime? StartDate { get; set; }
+        public string TerminationReason { get; set; }
+        public bool? OxygenUsed { get; set; }
+        public int? PeakId { get; set; }
+        public int? TrekkingAgencyId { get; set; }
+
+        public virtual Peak Peak { get; set; }
+        public virtual TrekkingAgency TrekkingAgency { get; set; }
+    }
+}
