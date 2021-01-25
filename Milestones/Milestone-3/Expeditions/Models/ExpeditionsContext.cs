cbda14d5230e0c3081772dbcb190c66b052479e4
Alex Howard <alexhowardcomputer@gmail.com>
2021-01-25 10:49:59 -0800

    tweak syntax in Azure-ready Expeditions project

--- Milestones/Milestone-3/Expeditions/Models/ExpeditionsContext.cs
+++ Milestones/Milestone-3/Expeditions/Models/ExpeditionsContext.cs
@@ -24,8 +24,8 @@ namespace Expeditions.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Expeditions;Trusted_Connection=True;");
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:expeditions-server.database.windows.net,1433;Initial Catalog=Expeditions;Persist Security Info=False;User ID=expeditionsadmin2021;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
