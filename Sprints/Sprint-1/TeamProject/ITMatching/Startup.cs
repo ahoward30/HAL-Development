e09e0cf8cc50e8e62f2ec13c0373e55526357c9b
Alex Howard <alexhowardcomputer@gmail.com>
2021-02-11 10:55:16 -0800

    Enable runtime compilation with Razor Pages, scaffold Login and Register classes, and comment out code about external login from those views

--- Sprints/Sprint-1/TeamProject/ITMatching/Startup.cs
+++ Sprints/Sprint-1/TeamProject/ITMatching/Startup.cs
@@ -35,6 +35,8 @@ namespace ITMatching
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            // Added to enable runtime compilation
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
