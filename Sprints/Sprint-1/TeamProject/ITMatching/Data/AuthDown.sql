7fb5a77897f818b7f537e3568f2c3ccc6baf4612
Alex Howard <alexhowardcomputer@gmail.com>
2021-02-12 17:54:17 -0800

    Rename UpdateIdentityAzure to AuthUp and add AuthDown.sql

---
+++ Sprints/Sprint-1/TeamProject/ITMatching/Data/AuthDown.sql
@@ -0,0 +1,9 @@
+-- DOWN script for SQL Server
+
+DROP TABLE [AspNetRoles];
+DROP TABLE [AspNetUsers];
+DROP TABLE [AspNetRoleClaims];
+DROP TABLE [AspNetUserClaims];
+DROP TABLE [AspNetUserLogins];
+DROP TABLE [AspNetUserRoles];
+DROP TABLE [AspNetUserTokens];
