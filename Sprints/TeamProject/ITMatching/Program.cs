10e1423348e837fce7184b6e3098de2ff56016b1
Jimmy Larios <jimmylarios96@gmail.com>
2021-03-06 01:03:53 -0800

    Edits and adds files to seed users automatically.

--- Sprints/TeamProject/ITMatching/Program.cs
+++ Sprints/TeamProject/ITMatching/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using ITMatching.Utilities;
+using ITMatching.Data;
 
 namespace ITMatching
 {
@@ -13,7 +16,40 @@ namespace ITMatching
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Older code will be split between Build() and Run()
+            // CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+            // After Build has been called, all services have been registered (by running Startup)
+            // By using a scope for the services to be requested below, we limit their lifetime to this set of calls.
+            // See: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-5.0#call-services-from-main
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Get the IConfiguration service that allows us to query user-secrets and
+                    // the configuration on Azure
+                    var config = host.Services.GetRequiredService<IConfiguration>();
+
+                    // Set password with the Secret Manager tool, or store in Azure app configuration
+                    // dotnet user-secrets set SeedUserPW <pw>
+
+                    var testUserPw = config["SeedUserPW"];
+                    //var adminPw = config["SeedAdminPW"]; //Part2
+
+                    SeedUsers.Initialize(services, SeedData.UserSeedData, testUserPw).Wait();
+                    //SeedUsers.InitializeAdmin(services, "admin@example.com", "admin", adminPw, "The", "Admin").Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured seeding the DB.");
+                }
+            }
+            // Go ahead and run the app, everything should be ready
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
