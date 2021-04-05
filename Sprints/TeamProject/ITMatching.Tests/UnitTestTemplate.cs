87e6bbf1a1975a1dfe25116f2e7a589607981b2a
Alex Howard <alexhowardcomputer@gmail.com>
2021-04-04 23:49:48 -0700

    Set up testing project

---
+++ Sprints/TeamProject/ITMatching.Tests/UnitTestTemplate.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using ITMatching.Controllers;
+
+namespace ITMatching.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
