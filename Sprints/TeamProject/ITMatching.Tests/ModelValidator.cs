6a9551c7c72cd91a6245eae250c9b46d29beedd1
Adn]an Almarzooq <cj-21-amm@hotmail.com>
2021-04-12 10:21:39 -0700

    removes unnecessary lines.

--- Sprints/TeamProject/ITMatching.Tests/ModelValidator.cs
+++ Sprints/TeamProject/ITMatching.Tests/ModelValidator.cs
@@ -16,17 +16,11 @@ namespace ITMatching.Tests
             Valid = Validator.TryValidateObject(model, vctx, Validations, true);
         }
 
-        /// <summary>
-        /// Check to see if the given member name, i.e. property name, was the cause of a validation result.
-        /// The version written here does not also check the reason why it failed.  That would be in the ErrorMessage
-        /// and would require parsing or listing that.  That's something that could be added to improve this class.
-        /// </summary>
-        /// <param name="member">The name of the member to check for, i.e. "Id", "Title", ...</param>
-        /// <returns>True if the member caused a validation result, False if it was not found among the results</returns>
+
         public bool ContainsFailureFor(string member) =>
             Validations.Select(vr => vr.MemberNames).Select(member => member.Where(term => member.Equals(term))).Any();
 
-        // The LINQ method Aggregate is the same as functional programming's "fold" or "reduce" function
+        
         // Get all errors in a single string
         public string GetAllErrors() =>
             Validations.Aggregate("", (accumulator, validation) => accumulator + $",{validation.ErrorMessage}");
