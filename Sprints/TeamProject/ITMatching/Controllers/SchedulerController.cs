de613b558e7c7513623c8abb385d790c5b3e38b3
Adn]an Almarzooq <cj-21-amm@hotmail.com>
2021-04-21 23:30:22 -0700

    edits Scheduler controller, last commit missed it.

--- Sprints/TeamProject/ITMatching/Controllers/SchedulerController.cs
+++ Sprints/TeamProject/ITMatching/Controllers/SchedulerController.cs
@@ -38,116 +38,220 @@ namespace ITMatching.Controllers
             string[] Sat = sch.Saturday;
             string[] Sun = sch.Sunday;
 
-            List<WorkSchedule> workHr = _context.WorkSchedules.Where(work => work.ExpertId == sch.UserId).ToList();
-            if (workHr != null)
+            string id = _userManager.GetUserId(User);
+            Itmuser itUser = _context.Itmusers.Where(u => u.AspNetUserId == id).FirstOrDefault();
+            Expert expert = _context.Experts.Where(u => u.ItmuserId == itUser.Id).FirstOrDefault();
+            Client client = _context.Clients.Where(u => u.ItmuserId == itUser.Id).FirstOrDefault();
+
+            if (expert != null)
             {
-                foreach (var item in workHr)
+                List<WorkSchedule> workHr = _context.WorkSchedules.Where(work => work.ExpertId == sch.UserId).ToList();
+                if (workHr != null)
                 {
-                    _context.WorkSchedules.Remove(item);
+                    foreach (var item in workHr)
+                    {
+                        _context.WorkSchedules.Remove(item);
+                    }
+                    _context.SaveChanges();
                 }
+                List<WorkSchedule> schedules = new List<WorkSchedule>();
+                foreach (var item in Mon)
+                {
+                    int h = item.IndexOf('_');
+
+                    WorkSchedule sc = new WorkSchedule()
+                    {
+                        ExpertId = sch.UserId,
+                        Day = "Monday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+                    schedules.Add(sc);
+                }
+                foreach (var item in Tue)
+                {
+                    int h = item.IndexOf('_');
+
+                    WorkSchedule sc = new WorkSchedule()
+                    {
+                        ExpertId = sch.UserId,
+                        Day = "Tuesday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+                    schedules.Add(sc);
+                }
+                foreach (var item in Wed)
+                {
+                    int h = item.IndexOf('_');
+
+                    WorkSchedule sc = new WorkSchedule()
+                    {
+                        ExpertId = sch.UserId,
+                        Day = "Wednesday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+                    schedules.Add(sc);
+                }
+                foreach (var item in Thu)
+                {
+                    int h = item.IndexOf('_');
+
+                    WorkSchedule sc = new WorkSchedule()
+                    {
+                        ExpertId = sch.UserId,
+                        Day = "Thursday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+                    schedules.Add(sc);
+                }
+                foreach (var item in Fri)
+                {
+                    int h = item.IndexOf('_');
+
+                    WorkSchedule sc = new WorkSchedule()
+                    {
+                        ExpertId = sch.UserId,
+                        Day = "Friday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+                    schedules.Add(sc);
+                }
+                foreach (var item in Sat)
+                {
+                    int h = item.IndexOf('_');
+
+                    WorkSchedule sc = new WorkSchedule()
+                    {
+                        ExpertId = sch.UserId,
+                        Day = "Saturday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+
+                    schedules.Add(sc);
+                }
+                foreach (var item in Sun)
+                {
+                    int h = item.IndexOf('_');
+
+                    WorkSchedule sc = new WorkSchedule()
+                    {
+                        ExpertId = sch.UserId,
+                        Day = "Sunday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+
+                    schedules.Add(sc);
+                }
+
+                _context.WorkSchedules.AddRange(schedules);
                 _context.SaveChanges();
             }
-            List<WorkSchedule> schedules = new List<WorkSchedule>();
-            foreach (var item in Mon)
+            else if (client != null)
             {
-                int h = item.IndexOf('_');
-
-                WorkSchedule sc = new WorkSchedule()
+                HelpRequest request = _context.HelpRequests.Where(u => u.ClientId == client.ItmuserId).OrderBy(u => u.Id).LastOrDefault();
+                List<RequestSchedule> schedules = new List<RequestSchedule>();
+                foreach (var item in Mon)
                 {
-                    ExpertId = sch.UserId,
-                    Day = "Monday",
-                    Hour = Int32.Parse(item.Substring(h + 1))
-                };
-                //_context.WorkSchedules.Add(sc);
-                schedules.Add(sc);
-            }
-            foreach (var item in Tue)
-            {
-                int h = item.IndexOf('_');
+                    int h = item.IndexOf('_');
 
-                WorkSchedule sc = new WorkSchedule()
-                {
-                    ExpertId = sch.UserId,
-                    Day = "Tuesday",
-                    Hour = Int32.Parse(item.Substring(h + 1))
-                };
-                //_context.WorkSchedules.Add(sc);
-                schedules.Add(sc);
-            }
-            foreach (var item in Wed)
-            {
-                int h = item.IndexOf('_');
+                    RequestSchedule sc = new RequestSchedule()
+                    {
+                        ClientId = sch.UserId,
+                        RequestId = request.Id,
+                        Day = "Monday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
 
-                WorkSchedule sc = new WorkSchedule()
+                    schedules.Add(sc);
+                }
+                foreach (var item in Tue)
                 {
-                    ExpertId = sch.UserId,
-                    Day = "Wednesday",
-                    Hour = Int32.Parse(item.Substring(h + 1))
-                };
-                //_context.WorkSchedules.Add(sc);
-                schedules.Add(sc);
-            }
-            foreach (var item in Thu)
-            {
-                int h = item.IndexOf('_');
+                    int h = item.IndexOf('_');
 
-                WorkSchedule sc = new WorkSchedule()
-                {
-                    ExpertId = sch.UserId,
-                    Day = "Thursday",
-                    Hour = Int32.Parse(item.Substring(h + 1))
-                };
-                //_context.WorkSchedules.Add(sc);
-                schedules.Add(sc);
-            }
-            foreach (var item in Fri)
-            {
-                int h = item.IndexOf('_');
+                    RequestSchedule sc = new RequestSchedule()
+                    {
+                        ClientId = sch.UserId,
+                        RequestId = request.Id,
+                        Day = "Tuesday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
 
-                WorkSchedule sc = new WorkSchedule()
+                    schedules.Add(sc);
+                }
+                foreach (var item in Wed)
                 {
-                    ExpertId = sch.UserId,
-                    Day = "Friday",
-                    Hour = Int32.Parse(item.Substring(h + 1))
-                };
-                //_context.WorkSchedules.Add(sc);
-                schedules.Add(sc);
-            }
-            foreach (var item in Sat)
-            {
-                int h = item.IndexOf('_');
+                    int h = item.IndexOf('_');
 
-                WorkSchedule sc = new WorkSchedule()
-                {
-                    ExpertId = sch.UserId,
-                    Day = "Saturday",
-                    Hour = Int32.Parse(item.Substring(h + 1))
-                };
-                //_context.WorkSchedules.Add(sc);
-                schedules.Add(sc);
-            }
-            foreach (var item in Sun)
-            {
-                int h = item.IndexOf('_');
+                    RequestSchedule sc = new RequestSchedule()
+                    {
+                        ClientId = sch.UserId,
+                        RequestId = request.Id,
+                        Day = "Wednesday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
 
-                WorkSchedule sc = new WorkSchedule()
+                    schedules.Add(sc);
+                }
+                foreach (var item in Thu)
                 {
-                    ExpertId = sch.UserId,
-                    Day = "Sunday",
-                    Hour = Int32.Parse(item.Substring(h + 1))
-                };
-                //_context.WorkSchedules.Add(sc);
-                schedules.Add(sc);
+                    int h = item.IndexOf('_');
+
+                    RequestSchedule sc = new RequestSchedule()
+                    {
+                        ClientId = sch.UserId,
+                        RequestId = request.Id,
+                        Day = "Thursday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+
+                    schedules.Add(sc);
+                }
+                foreach (var item in Fri)
+                {
+                    int h = item.IndexOf('_');
+
+                    RequestSchedule sc = new RequestSchedule()
+                    {
+                        ClientId = sch.UserId,
+                        RequestId = request.Id,
+                        Day = "Friday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+
+                    schedules.Add(sc);
+                }
+                foreach (var item in Sat)
+                {
+                    int h = item.IndexOf('_');
+
+                    RequestSchedule sc = new RequestSchedule()
+                    {
+                        ClientId = sch.UserId,
+                        RequestId = request.Id,
+                        Day = "Saturday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+
+                    schedules.Add(sc);
+                }
+                foreach (var item in Sun)
+                {
+                    int h = item.IndexOf('_');
+
+                    RequestSchedule sc = new RequestSchedule()
+                    {
+                        ClientId = sch.UserId,
+                        RequestId = request.Id,
+                        Day = "Sunday",
+                        Hour = Int32.Parse(item.Substring(h + 1))
+                    };
+                    //_context.WorkSchedules.Add(sc);
+                    schedules.Add(sc);
+                }
+
+                _context.RequestSchedules.AddRange(schedules);
+                _context.SaveChanges();
             }
-            /* using(ITMatchingAppContext context = new ITMatchingAppContext())
-             {
-                 context.WorkSchedules.AddRange(schedules);
-                 context.SaveChanges();
-             }*/
-            _context.WorkSchedules.AddRange(schedules);
-            _context.SaveChanges();
             return Ok();
-
         }
 
         [NonAction]
