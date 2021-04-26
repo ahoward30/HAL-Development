4f7f9acd664b59ce028d13df2ce20df361877ddb
Adnan Almarzooq <cj-21-amm@hotmail.com>
2021-04-26 10:33:54 -0700

    removes unessary commented lines.

--- Sprints/TeamProject/ITMatching/Models/Concrete/Repository.cs
+++ Sprints/TeamProject/ITMatching/Models/Concrete/Repository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ITMatching.Models.Concrete
 {
-    /// <summary>
+
     /// Base class repository that implements only CRUD operations.  Meant to be like an abstract 
     /// base class, but not actually made abstract because it may be useful to have a repository 
     /// that only supports crud.
@@ -18,15 +18,13 @@ namespace ITMatching.Models.Concrete
     ///      and have ModelBase define important things like the PK name and type so we can enforce that in
     ///      FindById for example.
     ///    - Non async versions
-    ///    - As Baltazar suggested, add a method called something like GetFiltered that takes Func delegate
-    ///      and allows us to apply a filter like Where when getting an IQueryable of TEntity
-    ///    - ?
-    /// </summary>
-    /// <typeparam name="TEntity">This is the entity for which we're making a repository</typeparam>
+    ///    - As Baltazar suggested during class, add a method called something like GetFiltered that takes Func delegate
+    ///      and allows us to apply a filter like Where when getting an IQueryable of TEntity.
+
+ 
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
     {
-        // If you want to enforce full separation and ensure that this repository ONLY accesses the TEntity
-        // then make _context private
+
         protected readonly DbContext _context;
         protected readonly DbSet<TEntity> _dbSet;
 
@@ -36,11 +34,10 @@ namespace ITMatching.Models.Concrete
             _dbSet = _context.Set<TEntity>();   // must do it this way because we don't have a "navigation property" to use
         }
 
-        // Find by ID assuming it's the PK and is an int
         public virtual async Task<TEntity> FindByIdAsync(int id)
         {
             TEntity entity = await _dbSet.FindAsync(id);
-            return entity;  // null if not found
+            return entity; 
         }
 
         public virtual async Task<bool> ExistsAsync(int id)
