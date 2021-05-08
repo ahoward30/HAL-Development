d63690e7bcdec4213fe3fb0259e9411a0f5d30e3
Adnan Almarzooq <cj-21-amm@hotmail.com>
2021-05-08 14:29:07 +0530

    adds unit tests for the delete account

---
+++ Sprints/TeamProject/ITMatching.Tests/FakeSignInManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMatching.Tests
+{
+    public class FakeSignInManager : SignInManager<IdentityUser>
+    {
+        public FakeSignInManager()
+                : base(new FakeUserManager(),
+                     new Mock<IHttpContextAccessor>().Object,
+                     new Mock<IUserClaimsPrincipalFactory<IdentityUser>>().Object,
+                     new Mock<IOptions<IdentityOptions>>().Object,
+                     new Mock<ILogger<SignInManager<IdentityUser>>>().Object,
+                     new Mock<IAuthenticationSchemeProvider>().Object,
+                     new Mock<IUserConfirmation<IdentityUser>>().Object)
+        { }
+
+        public override Task SignOutAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
