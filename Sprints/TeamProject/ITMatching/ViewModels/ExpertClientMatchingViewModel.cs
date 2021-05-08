c4f8619d1927c3341141f591ea271db2905109d0
Jimmy Larios <jimmylarios96@gmail.com>
2021-05-07 20:08:36 -0700

    Sets up viewmodel to be used in ExpertClientMatching view

---
+++ Sprints/TeamProject/ITMatching/ViewModels/ExpertClientMatchingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ITMatching.Models;
+using ITMatching.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ITMatching.ViewModels
+{
+    public class ExpertClientMatchingViewModel
+    {
+        Itmuser Itmuser { get; set; } //Name, email
+        Expert Expert { get; set; } //Schedule
+        public List<Service> Services { get; set; } // tags
+
+        List<(int, double)> OfflineExpertIdsAndScores { get; set; } // matching score
+    }
+}
