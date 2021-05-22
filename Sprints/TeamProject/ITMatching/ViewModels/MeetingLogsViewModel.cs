fae73ceb653b6c6a539f523a4c52d4bce69a2c88
Jimmy Larios <jimmylarios96@gmail.com>
2021-05-22 01:47:55 -0700

    Creates a viewmodel for meeting logs to pass into view page

---
+++ Sprints/TeamProject/ITMatching/ViewModels/MeetingLogsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ITMatching.Models;
+using ITMatching.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITMatching.ViewModels
+{
+    public class MeetingLogsViewModel
+    {
+        public Itmuser CurrentUser { get; set; } // User's FirstName and LastName
+        public List<Itmuser> MatchedUsers { get; set; }
+        public List<Service> Services { get; set; } // Help request tags
+        public List<Meeting> Meetings { get; set; } // Meeting date and time
+        public List<HelpRequest> HelpRequests { get; set; } // Help request title and description
+        public List<RequestService> RequestServices { get; set; } // Link services to help request
+        public List<MeetingLogsViewModel> MeetingLogModel { get; set; }
+    }
+}
