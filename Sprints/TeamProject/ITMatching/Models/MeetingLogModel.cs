d363ce4efef963c12afebaa365dd0eb63cda2b99
Jimmy Larios <jimmylarios96@gmail.com>
2021-05-24 03:12:19 -0700

    Modifies the model and viewmodel to be used in MeetingLog view page

--- Sprints/TeamProject/ITMatching/Models/MeetingLogModel.cs
+++ Sprints/TeamProject/ITMatching/Models/MeetingLogModel.cs
@@ -6,30 +6,27 @@ using System.Threading.Tasks;
 namespace ITMatching.Models
 {
 
-    public partial class MeetingLogModel
+    public class MeetingLogModel
     {
-        public MeetingLogModel()
-        {
-            RequestTitle = "Title";
-            RequestDescription = "Description";
-            MatchedUserName = "UserName";
-            MatchedUserEmail = "Email";
-            TimeOfMeeting = DateTime.UtcNow;
-            HelpRequestTags = new List<Service>();
-            meetingId = 0;
-            isExpert = false;
-        }
+        //public MeetingLogModel()
+        //{
+        //    RequestTitle = "Title";
+        //    RequestDescription = "Description";
+        //    MatchedUserName = "UserName";
+        //    MatchedUserEmail = "Email";
+        //    TimeOfMeeting = DateTime.UtcNow;
+        //    HelpRequestTags = new List<Service>();
+        //    MeetingId = 0;
+        //    IsExpert = false;
+        //}
 
-//        public List<MeetingLogModel>
-//        {
-//}
         public string RequestTitle { get; set; }
         public string RequestDescription { get; set; }
         public string MatchedUserName { get; set; }
         public string MatchedUserEmail { get; set; }
         public DateTime TimeOfMeeting { get; set; }
         public List<Service> HelpRequestTags { get; set; } = new List<Service>();
-        public int meetingId { get; set; }
-        public bool isExpert { get; set; }
+        public int MeetingId { get; set; }
+        public bool IsExpert { get; set; }
     }
 }
