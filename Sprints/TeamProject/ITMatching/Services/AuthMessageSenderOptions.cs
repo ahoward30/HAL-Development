fd19ca8aa21f23238b96b402e036dfda8030ac56
Jimmy Larios <jimmylarios96@gmail.com>
2021-05-29 21:19:56 -0700

    Changes property names for API key and user

--- Sprints/TeamProject/ITMatching/Services/AuthMessageSenderOptions.cs
+++ Sprints/TeamProject/ITMatching/Services/AuthMessageSenderOptions.cs
@@ -7,7 +7,7 @@ namespace ITMatching.Services
 {
     public class AuthMessageSenderOptions
     {
-        public string SendGridUser { get; set; }
-        public string SendGridKey { get; set; }
+        public string SGUser { get; set; }
+        public string SGKey { get; set; }
     }
 }
