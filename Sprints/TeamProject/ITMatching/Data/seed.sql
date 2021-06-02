5e93b30bbd0db974b8b7df2d8096e7b0ae3a0d99
Jimmy Larios <jimmylarios96@gmail.com>
2021-06-01 23:16:31 -0700

    Adds new seed data for FAQ page

--- Sprints/TeamProject/ITMatching/Data/seed.sql
+++ Sprints/TeamProject/ITMatching/Data/seed.sql
@@ -77,20 +77,15 @@ INSERT INTO [Service](ServiceCategory,ServiceName) VALUES('CommunicationMethod',
 INSERT INTO [Service](ServiceCategory,ServiceName) VALUES('CommunicationMethod', 'Screenshare');
 
 --Insert FAQ Page Questions and Answers
-INSERT INTO [FAQ](Question,Answer) VALUES('How do I delete my account?', 'At the moment you cannot delete your account. Our staff is currently working on implementing this feature.');
-INSERT INTO [FAQ](Question,Answer) VALUES('How do I create a post?', 'You must be a registered user to create a post. To create a post, hover over to the top-right corner and click on the create post button.');
-INSERT INTO [FAQ](Question,Answer) VALUES('Where can I go to register for an account?', 'Click on the "Sign In" button at the top. Then click on the "create an account" link highlighted in blue.');
-INSERT INTO [FAQ](Question,Answer) VALUES('Why are my posts not appearing on screen?', 'Try refreshing your page after submitting a post.');
-INSERT INTO [FAQ](Question,Answer) VALUES('Can I register as both a user and an expert?', 'Yes, you may sign up as a user and an expert.');
-INSERT INTO [FAQ](Question,Answer) VALUES('How do I cancel my meeting with an expert instructor?', 'At the moment you cannot cancel your account. Our staff is currently working on implementing this feature.');
-INSERT INTO [FAQ](Question,Answer) VALUES('Can I choose more than one professional instructor?', 'No, you must select only one instructor of the options you are given.');
-INSERT INTO [FAQ](Question,Answer) VALUES('Can I accept more than one client to instruct?', 'Yes, you certainly may. However, keep in mind not to have conflicting schedules with your clients.');
-INSERT INTO [FAQ](Question,Answer) VALUES('As an expert, is there an option to let a user know why I specifically refused their request?', 'At the moment you cannot let the user know. Our staff is currently working on implementing this feature.');
-INSERT INTO [FAQ](Question,Answer) VALUES('Can I select the same expert again without having to go through a match-up?', 'Yes, but at the moment you cannot. Our staff is currently working on fixing the bug to this feature.');
-INSERT INTO [FAQ](Question,Answer) VALUES('Can I recieve notifications so that I may be informed if I got accepted by an expert?', 'At the moment you cannot recieve notifications to your account. Our staff is currently working on implementing this feature.');
-INSERT INTO [FAQ](Question,Answer) VALUES('I forgot the scheduled meeting time with my expert instructor, where can I see this?', 'At the moment you cannot see your schedule. Our staff is currently working on implementing this feature..');
-INSERT INTO [FAQ](Question,Answer) VALUES('Is there a fee to sign up as a client user?', 'No, there a no fees, as well as hidden fees, when you create a client account.');
-INSERT INTO [FAQ](Question,Answer) VALUES('Is it possible not to be matched with an expert?', 'No. We have created a selection system in which you will be paired up with at least one expert.');
-INSERT INTO [FAQ](Question,Answer) VALUES('My question is not answered here', 'Try contacting us by phone at 1-503-555-5555 or by email at HAL@example-email.com');
+INSERT INTO [FAQ](Question,Answer) VALUES('What is the difference if I sign up as a Client compared to if I sign up as an Expert?', 'As a client, you can request help from an Expert to assist you in solving your IT issue. An Expert can choose a matching Client they want to assist with their IT issue.');
+INSERT INTO [FAQ](Question,Answer) VALUES('If I am already registered as an Expert, do I have to sign up as a Client to send a help request to another Expert?', 'Yes. You must create a separate account (i.e. register as a Client) to receive help from another Expert.');
+INSERT INTO [FAQ](Question,Answer) VALUES('As a Client, can I submit multiple help requests at the same time?', 'No, you can only submit one help request at a time. Attempting to submit another help request will close the previous one you made.');
+INSERT INTO [FAQ](Question,Answer) VALUES('What do I if I submit a help request and I do not get matched up with an Expert?', 'You can re-submit your help request and try adding more tags that specify your IT issue. If you still see no results then there might not be an Expert who can help you out.');
+INSERT INTO [FAQ](Question,Answer) VALUES('As an Expert, can I accept multiple clients to assist?', 'No, you may only choose to help out one client at a time.');
+INSERT INTO [FAQ](Question,Answer) VALUES('Can I delete my account?', 'Yes. You can head over to your account profile (link in the top-right hand corner of the page) and select the Delete Account tab to enter the page to remove your account.');
+INSERT INTO [FAQ](Question,Answer) VALUES('I am unable to verify my email?', 'Make sure to check all your folders including your spam folder in your email inbox. You may also go into the login page and click on the Resend Email Confirmation link to receive another verification link.');
+INSERT INTO [FAQ](Question,Answer) VALUES('I forgot my password can I reset it?', 'Yes. Head over to the login page and select the Forgot your password? link. Enter in your email so that can send you a link to change your password.');
+INSERT INTO [FAQ](Question,Answer) VALUES('Can I change my email at any time?', 'Yes. You can change your email by selecting the Change Email tab on your account profile page. Do not forget to check your new email inbox for an email verification link.');
+INSERT INTO [FAQ](Question,Answer) VALUES('My question is not answered here.', 'Contact us by email at itmatching@outlook.com');
 
 --Insert Test Meeting objects to test Expert Waiting Room
