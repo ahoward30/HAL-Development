dd708201bfe9deaed43b6d2245abc84c28513b0a
Alex Howard <alexhowardcomputer@gmail.com>
2021-06-09 14:41:00 -0700

    Implement BDD test for switching between site themes

---
+++ Sprints/TeamProject/ITMatching.BDDTests/Features/ThemeToggle.feature
@@ -0,0 +1,8 @@
+﻿Feature: ThemeToggle
+	Slider on the top of the page for switching between light and dark themes
+
+Scenario: Toggle the site theme
+	Given I am on the home page
+	And the current theme is dark
+	When I click on the theme slider
+	Then the theme should change to be light
---
+++ Sprints/TeamProject/ITMatching.BDDTests/Steps/ThemeToggleStepDefinitions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using ITMatching.Models;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using System.Text;
+using System.Threading;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ITMatching.BDDTests.Steps
+{
+    [Binding]
+    public class ThemeToggleSteps
+    {
+        private readonly ScenarioContext _ctx;
+        private string _hostBaseName = @"https://it-matching.azurewebsites.net/";
+        private IWebDriver driver = new ChromeDriver();
+
+        public ThemeToggleSteps(ScenarioContext scenarioContext)
+        {
+            _ctx = scenarioContext;
+            _ctx["WebDriver"] = driver;
+        }
+
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            IWebDriver driver = (IWebDriver)_ctx["WebDriver"];
+            driver.Navigate().GoToUrl(_hostBaseName);
+        }
+
+        [Given(@"the current theme is dark")]
+        public void GivenTheCurrentThemeIsDark()
+        {
+            bool isLightTheme = driver.FindElement(By.Id("toggleButton")).Selected;
+            if (isLightTheme)
+            {
+                driver.FindElement(By.ClassName("switch")).Click();
+            }
+        }
+        
+        [When(@"I click on the theme slider")]
+        public void WhenIClickOnTheThemeSlider()
+        {
+            driver.FindElement(By.ClassName("switch")).Click();
+        }
+
+        [Then(@"the theme should change to be light")]
+        public void ThenTheThemeShouldChangeToBeLight()
+        {
+            bool isLightTheme = driver.FindElement(By.Id("toggleButton")).Selected;
+            Assert.That(isLightTheme);
+        }
+    }
+}
